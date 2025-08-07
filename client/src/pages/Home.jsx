df650c37c9b0bdfbcb990b17ad1056a2c7704428
unknown <crystalknt205@gmail.com>
2025-08-07 21:33:32 +0630

    Background

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 function Home() {
   return (
-    <div>
-      {/* <h1>Home</h1> */}
-    </div>
+    <>
+      <HeroSection />
+    </>
   )
 }
 
